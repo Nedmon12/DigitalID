c0d3b0cef9e35f5ef9afbc4b67e9f15e4a37f2f9
Nedmon12 <mentesnot.ertibu@gmail.com>
2023-05-30 11:49:50 +0300

    postgres container -- prisma init

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import { Request, Response, NextFunction } from 'express'
 import { buildToken } from '../middlewares/auth'
+import { PrismaClient } from '@prisma/client'
 
+
+const prisma = PrismaClient
 export function login (req: Request, res: Response) {
     const {username, password} = req.body
     //
-    const user = db.findOne({username: username}) //I know db is undefined right now haven't decided yet
+    // const user = db.findOne({username: username}) //I know db is undefined right now haven't decided yet
+    const user = await prisma.user.findeOne()
     if (!user) {
         //user not found
         return res.status(400).json({"error": "user not found"})
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 
 import express from 'express'
 import { Request, Response, NextFunction } from 'express'
+import router from './routes/routes'
 const app = express()
 const PORT = process.env.PORT || 7000
+app.get('/api', router)
+app.get('/healthCheck', (req: Request, res: Response)=> {
+    res.send(200).json({"health status": "okay"})
+})
 app.listen(PORT, ()=> {
     console.log(`App is running on ${PORT}`)
 })
